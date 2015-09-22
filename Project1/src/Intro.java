fca004155694412ec402c006b7fbf8527a09aaa3
priab <pbrown@atl.yearup.org>
2015-09-22 12:15:14 -0400

    First Java file

---
+++ Project1/src/Intro.java
@@ -0,0 +1,10 @@
+
+public class Intro 
+{
+	public static void main(String[] args) 
+	{
+		 System.out.println("My name is Pria.");
+		 System.out.println("My favorite movie is Human Centipede.");
+		 System.out.println("My favorite color is purple.");	
+	}
+}
